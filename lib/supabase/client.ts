b708fde0eb52399e7c393c84790eef859d31af2b
Sakshi <bhagwatisoni731@gmail.com>
2025-12-22 23:23:11 +0530

    Debugging keys

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,3 +3,5 @@ import { createBrowserClient } from "@supabase/ssr"
 export function createClient() {
   return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 }
+console.log("CHECK URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("CHECK KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
